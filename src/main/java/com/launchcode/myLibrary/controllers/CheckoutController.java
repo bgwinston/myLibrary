149a48cff1b888bf6c4fbbe5ee60978fb588b885
Britaney <b.winston87@yahoo.com>
2020-03-28 22:05:00 -0500

    results template added to checkout template folder

--- src/main/java/com/launchcode/myLibrary/controllers/CheckoutController.java
+++ src/main/java/com/launchcode/myLibrary/controllers/CheckoutController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.awt.print.Book;
@@ -16,10 +17,17 @@ public class CheckoutController {
     @Autowired
     private BookDao bookDao;
     //Checkout b
-    @RequestMapping(value = "")
+    @RequestMapping(value = "", method= RequestMethod.GET)
     public String displaycheckout(Model model){
         model.addAttribute("title","Check out Book");
         model.addAttribute("book", bookDao.findAll());
         return "home/checkout";
+    }
+
+    @RequestMapping(value="results", method=RequestMethod.GET)
+    public String displayResults(Model model){
+        model.addAttribute("title", "Search Results");
+        model.addAttribute("book", bookDao.findAll());
+        return "checkout/results";
     }
 }
